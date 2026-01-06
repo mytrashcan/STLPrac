e24b5e7719886b9510176373456ccfc52e8bfe12
KitaeJeong <wjdrlxo2013@gmail.com>
2026-01-06 22:21:31 +0900

    added comment

--- BOJ/StringFrequency.cpp
+++ BOJ/StringFrequency.cpp
@@ -1,3 +1,23 @@
+/*
+문제 설명:
+투표 용지 N장이 있습니다. 각 용지에는 후보자의 이름(문자열)이 적혀 있습니다.
+누가 몇 표를 받았는지 세어서 출력하세요. (이름 사전 순으로 출력)
+
+입력 예시:
+
+5
+Tom
+Jerry
+Tom
+Tom
+Mickey
+
+출력 예시:
+
+Jerry: 1
+Mickey: 1
+Tom: 3
+*/
 #include <iostream>
 #include <string>
 #include <map>
